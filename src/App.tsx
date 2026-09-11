b1ce9ade5d76766464b280d8b1b2c399c54bca24
oscar-abud <oscarpalma.vpr2@gmail.com>
2026-09-11 13:43:46 -0300

    feat: add Tailwind CSS integration and global styles
    - Updated App component to include flex class for next-steps section.
    - Imported global CSS file in main entry point.
    - Modified TypeScript configuration to include path aliases.
    - Enhanced Vite configuration to support Tailwind CSS and path aliases.
    - Created a new globals.css file to import Tailwind CSS.

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ function App() {
 
       <div className="ticks"></div>
 
-      <section id="next-steps">
+      <section id="next-steps" className="flex">
         <div id="docs">
           <svg className="icon" role="presentation" aria-hidden="true">
             <use href="/icons.svg#documentation-icon"></use>
